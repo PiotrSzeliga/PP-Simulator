6b658248875e9526425da5108be51305dd299c22
Piotr Szeliga <piotrszeliga87@gmail.com>
2024-11-19 19:45:07 +0100

    Testy

--- TestSimulator/RectangleTests.cs
+++ TestSimulator/RectangleTests.cs
@@ -13,42 +13,51 @@ namespace TestSimulator;
 
 public class RectangleTests
 {
-    [Theory]
-    [InlineData(1, 2, 3, 4, 1, 2, 3, 4)]
-    [InlineData(1, 4, 2, 3, 1, 3, 2, 4)]
-    public void Constructor_ValidCoordinates_ShouldSetCoordinates(int x1, int y1, int x2, int y2, int exppectedX1, int exppectedY1, int exppectedX2, int exppectedY2)
+    [Fact]
+    public void Constructor_ValidCoordinates_ShouldSetCoordinates()
     {
+        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
         var rectangle = new Rectangle(x1, y1, x2, y2);
-        Assert.Equal((exppectedX1, exppectedY1, exppectedX2, exppectedY2), (rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
+        Assert.Equal((x1, y1, x2, y2),(rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
     }
 
-    [Theory]
-    [InlineData(1, 2, 3, 4, 1, 2, 3, 4)]
-    [InlineData(1, 4, 2, 3, 1, 3, 2, 4)]
-    public void Constructor_ValidPoints_ShouldSetCoordinates(int x1, int y1, int x2, int y2, int exppectedX1, int exppectedY1, int exppectedX2, int exppectedY2)
+    [Fact]
+    public void Constructor_ValidPoints_ShouldSetCoordinates()
     {
-        Point p1 = new Point(x1, y1);
-        Point p2 = new Point(x2, y2);
+        Point p1 = new Point(1, 2), p2 = new Point(3, 4);
         var rectangle = new Rectangle(p1, p2);
-        Assert.Equal((exppectedX1, exppectedY1, exppectedX2, exppectedY2), (rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
+        Assert.Equal((p1.X, p1.Y, p2.X, p2.Y), (rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
     }
 
-    [Theory]
-    [InlineData(1, 2, 1, 4)]
-    [InlineData(1, 6, 2, 6)]
-    public void Constructor_InvalidCoordinates_ShouldThrowArgumentException(int x1, int y1, int x2, int y2)
+    [Fact]
+    public void Constructor_WrongOrderCoordinates_ShouldSetProperCoordinates()
     {
+        int x1 = 1, y1 = 6, x2 = 3, y2 = 4;
+        var rectangle = new Rectangle(x1, y1, x2, y2);
+        Assert.Equal((x1, y2, x2, y1), (rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
+    }
+
+    [Fact]
+    public void Constructor_WrongOrderPoints_ShouldSetProperCoordinates()
+    {
+        Point p1 = new Point(7,6), p2 = new Point(3, 4);
+        var rectangle = new Rectangle(p1, p2);
+        Assert.Equal((p2.X, p2.Y, p1.X, p1.Y), (rectangle.X1, rectangle.Y1, rectangle.X2, rectangle.Y2));
+    }
+
+    [Fact]
+    public void Constructor_InvalidCoordinates_ShouldThrowArgumentException()
+    {
+        int x1 = 1, y1 = 2, x2 = 1, y2 = 4;
         Assert.Throws<ArgumentException>(() =>
              new Rectangle(x1, y1, x2, y2));
     }
 
-    [Theory]
-    [InlineData(1, 2, 1, 4)]
-    [InlineData(5, 6, 5, 8)]
-    public void Constructor_InvalidPoints_ShouldThrowArgumentException(int x1, int y1, int x2, int y2)
+    [Fact]
+    public void Constructor_InvalidPoints_ShouldThrowArgumentException()
     {
-        Point p1 = new Point(x1, y1);
-        Point p2 = new Point(x2, y2);
+        Point p1 = new Point(1, 4);
+        Point p2 = new Point(3, 4);
         Assert.Throws<ArgumentException>(() =>
              new Rectangle(p1, p2));
     }
@@ -67,8 +76,8 @@ public class RectangleTests
     [Fact]
     public void ToString_ShouldReturnCorrectString()
     {
-        int X1 = 1, Y1 = 2, X2 = 3, Y2 = 4;
-        var rectangle = new Rectangle(X1, Y1, X2, Y2);
-        Assert.Equal($"({X1}, {Y1}):({X2}, {Y2})", rectangle.ToString());
+        int x1 = 1, y1 = 2, x2 = 3, y2 = 4;
+        var rectangle = new Rectangle(x1, y1, x2, y2);
+        Assert.Equal($"({x1}, {y1}):({x2}, {y2})", rectangle.ToString());
     }
 }
